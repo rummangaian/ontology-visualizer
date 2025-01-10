f7c4484ab10d97c891bb76348599836d4cdfd9fa
rummangaian <mohammad.r@mobiusdtaas.ai>
2025-01-10 14:59:56 +0530

    added classes distance property and labels length property in the ontology viewer

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import {
 } from "@mobius/ontology-visualizer";
 import data from "../data/goodRelation.json";
 import "@mobius/ontology-visualizer/dist/index.css";
-import "./App.css"
+import "./App.css";
 import FilterButton from "./components/FilterButton/FilterButton";
 
 export default function App() {
   const graphRef = useRef(null);
   const viewerRef = useRef(null);
   const [degree, setDegree] = useState({ maxDegree: 0, currentDegree: 0 });
-
+  const [classDistance, setClassDistance] = useState(10);
+  const [labelLength , setLabelLength] = useState(30)
 
   const [selectedFilters, setSelectedFilters] = useState([]);
-  const [play , setPlay] = useState(true)
+  const [play, setPlay] = useState(true);
 
   // Maintain a single instance of options object
   const [options] = useState(
@@ -43,7 +44,10 @@ export default function App() {
       viewer.import(data);
 
       viewerRef.current = viewer;
-      
+
+      options.setClassDistance(10);
+
+      viewer.updateStyle();
     }
   }, [options]);
 
@@ -57,15 +61,24 @@ export default function App() {
     }
   };
 
-  const applyFilter = () => {
+  const applyFilter = (selectedFilters) => {
     const viewer = viewerRef.current;
-  
+
     if (viewer) {
       // Define all the possible filters that the viewer can handle
       const allFilters = [
-        "DISJOINT" , "DATATYPE" , "EXTERNAL" , "OBJECT" , "SUBCLASS" , "SET_OPERATOR" , "COMPACT_NOTATION" , "EMPTY_LITERAL" , "NODE_DEGREE"  , "STATISTICS"
+        "DISJOINT",
+        "DATATYPE",
+        "EXTERNAL",
+        "OBJECT",
+        "SUBCLASS",
+        "SET_OPERATOR",
+        "COMPACT_NOTATION",
+        "EMPTY_LITERAL",
+        "NODE_DEGREE",
+        "STATISTICS",
       ];
-  
+
       // Iterate over all filters and dynamically set their values
       allFilters.forEach((filter) => {
         const isActive = selectedFilters.includes(filter); // Check if the filter is in selectedFilters
@@ -77,39 +90,79 @@ export default function App() {
   };
 
   const handlePauseGraph = () => {
-    const viewer = viewerRef.current
-    if(viewer){
-      if(play) {
+    const viewer = viewerRef.current;
+    if (viewer) {
+      if (play) {
         viewer.pause();
-        setPlay(false)
-      }
-      else {
+        setPlay(false);
+      } else {
         viewer.resume();
-        setPlay(true)
+        setPlay(true);
       }
+    }
+  };
+
+  const applyClassDistance = (val) => {
+    setClassDistance(val); // Update state
+    const viewer = viewerRef.current;
+    if (viewer) {
+      options.setClassDistance(val); 
+      viewer.updateStyle(); 
+    }
+  };
+
+  const applyLabelLength = (val) => {
+    setLabelLength(val)
+    const viewer = viewerRef.current;
+    if (viewer) {
+      options.setDynamicWidth(true);
+      options.setMaxLabelWidth(parseInt(val));
+      viewer.animateDynamicLabelWidth();
     }
   }
 
-  useEffect(()=>{
-    applyFilter()
-  },[selectedFilters])
-  
+  useEffect(() => {
+    selectedFilters && applyFilter(selectedFilters);
+  }, [selectedFilters]);
 
   return (
     <div className="onto-container">
       <div className="onto-title">Mobius Ontology Viewer</div>
-      <div className="btns">
-      </div>
+      <div className="btns"></div>
       <div id="graph" ref={graphRef}></div>
       <div className="onto-setting-bar">
-        <FilterButton selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} degree={degree} applyNodeDegree={applyNodeDegree}/>
+        <FilterButton
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
+          degree={degree}
+          applyNodeDegree={applyNodeDegree}
+        />
+        <div>
+          <span>Class Distance</span>
+          <input
+            type="range"
+            min={10}
+            max={1000}
+            step={10}
+            onChange={(e) => applyClassDistance(parseInt(e.target.value))}
+            value={classDistance}
+          />
+        </div>
+        <div>
+          <span>Labels</span>
+          <input
+            type="range"
+            min={10}
+            max={100}
+            step={10}
+            onChange={(e) => applyLabelLength(parseInt(e.target.value))}
+            value={labelLength}
+          />
+        </div>
 
-      <button className="onto-setting-right-part" onClick={handlePauseGraph}>
-        {
-          play ? 
-          <>Pause</> : <>Play</>
-        }
-      </button>
+        <button className="onto-setting-right-part" onClick={handlePauseGraph}>
+          {play ? <>Pause</> : <>Play</>}
+        </button>
       </div>
     </div>
   );
